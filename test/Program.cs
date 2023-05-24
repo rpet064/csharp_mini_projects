a7b2000a9dcfd9f38a9f67c710ecc3d6c9cf5287
rpet064 <93119631+rpet064@users.noreply.github.com>
2023-05-24 12:38:41 +1200

    add fizzbuzz challenge

---
+++ test/Program.cs
@@ -0,0 +1,32 @@
+﻿// Fizzbuzz
+
+namespace FizzBuzz
+{
+    class Program {
+
+        public string FizzBuzz(int number){
+            string output = "";
+            
+            if (number % 3 == 0){
+                output += "fizz";
+            } 
+            if (number % 5 == 0){
+                output += "buzz";
+            }
+            if (output.Length == 0){
+                return number.ToString();
+            }
+            return output;
+        }
+    static void Main()
+        {
+            string num;
+            Program fizzBuzz = new Program();
+            for (int i = 0; i < 100; i++){
+                num = fizzBuzz.FizzBuzz(i);
+                Console.WriteLine(num);
+
+            }
+        }
+    }
+}
